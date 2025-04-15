4fbd23d5baf705c14dafdb2b7ff94a6e28e4a9d1
Daniel Alejandro Hernández Llerena <dhdezllerena94@gmail.com>
2025-04-15 08:04:59 +0100

    chore(docker-prisma): setup docker and prisma schema

---
+++ prisma/migrations/20250415065121_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Place" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "latitude" DOUBLE PRECISION NOT NULL,
+    "longitude" DOUBLE PRECISION NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Place_pkey" PRIMARY KEY ("id")
+);
